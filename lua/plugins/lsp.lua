0b3a60713a59865572d46ccc85c6d49a4a1a7b92
GamingGuy003 <bjornfelixh@gmail.com>
2025-10-01 13:51:10 +0200

    Update plugins

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,13 @@
+return {
+	"neovim/nvim-lspconfig",
+	opts = {
+		servers = {
+			rust_analyzer = {},
+			bashls = {},
+			cssls = {},
+			fish_lsp = {},
+			lua_ls = {},
+			markdown_oxide = {},
+		},
+	},
+}
